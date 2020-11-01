c02e5f212cb73da79c6b0ae07cd3b34cf4d9da1d
abnuo <66645994+abnuo@users.noreply.github.com>
2020-11-02 06:20:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import http.server
 import socketserver
 
 PORT = randrange(8000, 8999, 2)
+print("ill be alive at http://18.223.24.247" + str(PORT))
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("http://18.223.24.247" + str(PORT))
+    print("im alive at http://18.223.24.247" + str(PORT))
     httpd.serve_forever()
