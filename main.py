a0b81a56b393f2ae622e644fbe50f478ce0302fe
abnuo <66645994+abnuo@users.noreply.github.com>
2020-11-03 03:00:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import http.server
 import socketserver
 import random
 import logging
+from subprocess import Popen
 
 logging.basicConfig(filename='loggiez.log', format='%(levelname)s:%(message)s', level=logging.DEBUG)
 PORT = 8069
@@ -12,3 +13,4 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("im alive at http://18.223.24.247" + str(PORT))
     httpd.serve_forever()
+    p = Popen(['python', 'app.py'])
