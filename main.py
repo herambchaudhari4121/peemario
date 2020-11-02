cdabc61ba078b48862504a0878d8876b4136c03f
abnuo <66645994+abnuo@users.noreply.github.com>
2020-11-03 03:08:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ PORT = 8069
 print("ill be alive at http://18.223.24.247" + str(PORT))
 
 Handler = http.server.SimpleHTTPRequestHandler
-p = Popen(['python', 'app.py'])
+p = Popen(['python3', 'app.py'])
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("im alive at http://18.223.24.247" + str(PORT))
